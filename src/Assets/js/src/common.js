742ebc596172a31c918472c85e45373d8ca72f90
frankfoerster <github@frankfoerster.com>
2016-05-19 21:24:41 +0200

    add asset layout for js and less files

---
+++ src/Assets/js/src/common.js
@@ -0,0 +1,33 @@
+require.config({
+  config: {
+    moment: {
+      noGlobal: true
+    }
+  },
+  paths: {
+    backbone: '../vendor/backbone/backbone',
+    marionette: '../vendor/marionette/lib/backbone.marionette',
+    jquery: '../vendor/jquery/dist/jquery',
+    underscore: '../vendor/underscore/underscore'
+  },
+  packages: [
+    {
+      name: 'feature-detects',
+      location: '../../../node_modules/modernizr/feature-detects'
+    }
+  ],
+  shim: {
+    backbone: [
+      'underscore',
+      'jquery'
+    ],
+    marionette: [
+      'backbone'
+    ]
+  }
+});
+
+// removed in production by uglify
+if (typeof DEBUG === 'undefined') {
+  DEBUG = true;
+}
---
+++ src/Assets/js/src/main.js
@@ -0,0 +1,3 @@
+define(function (require) {
+  // App initialization logic
+});
